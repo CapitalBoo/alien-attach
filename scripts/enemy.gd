8122341eb1abe13bdd8899f9d53261708b9c4e07
CapitalBoo <CapitalBoo@gmail.com>
2023-07-04 21:08:24 -0400

    complete lesson 41

---
+++ scripts/enemy.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var enemy_speed = 200
+@onready var visible_container = $VisibleNotifier
+
+func _ready():
+	visible_container.connect("screen_exited", _on_screen_exited)
+
+func _physics_process(delta):
+	global_position.x += -enemy_speed*delta
+
+func _on_screen_exited():
+	queue_free()
---
+++ scripts/rocket.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var rocket_speed = 300
+@onready var visible_container = $VisibleNotifier
+
+func _ready():
+	visible_container.connect("screen_exited", _on_screen_exited)
+
+func _physics_process(delta):
+	global_position.x += rocket_speed*delta
+
+func _on_screen_exited():
+	queue_free()
