a6d500b4214c6a80b9374ceeafddbfb62a555202
CapitalBoo <CapitalBoo@gmail.com>
2023-07-04 21:54:04 -0400

    complete lesson 43

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-@onready var timer_container = $Timer
-
-#func _ready():
-#	timer_container.connect("timeout", spawn())
+@onready var spawn_positions = $SpawnPositions
+var enemy_scene = preload("res://scenes/enemy.tscn")
 
 func _on_timer_timeout():
-	spawn()
+	spawn_enemy()
 
-func spawn():
-	print("spawm me an alien")
+func spawn_enemy():
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	var enemy_instance = enemy_scene.instantiate()
+	enemy_instance.global_position = random_spawn_position.global_position
+	add_child(enemy_instance)
+#	print("spawm me an alien")
