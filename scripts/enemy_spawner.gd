dd1d7bdb9234b4a4ec07c527b89f1dd9472c164f
CapitalBoo <CapitalBoo@gmail.com>
2023-07-04 21:35:12 -0400

    complete lesson 42

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+@onready var timer_container = $Timer
+
+#func _ready():
+#	timer_container.connect("timeout", spawn())
+
+func _on_timer_timeout():
+	spawn()
+
+func spawn():
+	print("spawm me an alien")
